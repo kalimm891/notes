6424c3c5393f77163f6dbacd9c285b69a68689cc
Kalim Alam <kalimies1234@gmail.com>
2023-01-21 17:29:39 +0530

    first-commit

--- server.js
+++ server.js
@@ -2,17 +2,19 @@
 //initialization
 const express = require('express');
 const app = express();
-
+var morgan = require('morgan');
 const mongoose = require('mongoose');
-
 const bodyParser = require('body-parser');
+
+
+
 
 //inconming data handlers
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
-
 const Note = require('./src/models/Note');
+
+
 
 // nodejs connect to mongodb
 const mongoUrl = 'mongodb+srv://kalimm891:Kalim123@cluster0.b4rif7y.mongodb.net/notes';
@@ -30,22 +32,25 @@ app.get('/notes/list', async function(req, res){
 
     const notes = await Note.find();
     console.log("this is notes page");
-    res.json(notes);
+    res.status(200). json(notes);
 });
+
+
 
 
 //find data by id 
 app.get('/notes/list/:userid',async function(req, res){
-
     var notes = await Note.find({ userid:req.params.userid});
-    res.json(notes);
-    
+    res.status(201). json(notes);
 });
+
+
+
+
 
 //add data
 app.post('/notes/add', async function(req, res){
     await Note.deleteOne({ id: req.body.id});
-
     const newnotes = Note(
         {
         id:req.body.id,
@@ -57,10 +62,11 @@ app.post('/notes/add', async function(req, res){
    const noteData = await newnotes.save();
     const response = {
          message: "added", };
-    res.json(response);   
+    res.status(200).json(response);   
 });
     }
 );
+
 
 
 //delete notes
@@ -68,16 +74,19 @@ app.post('/notes/delete',async function(req, res){
     await Note.deleteOne({ id: req.body.id});
     const response = {
         message: "deleted", };
-    res.json(response);
-    
+    res.status(200).json(response);
 });
 
 
 
 
-//server created
-const port = process.env.PORT || 8080;
-app.listen(port, ()=>{
+
+
+
+
+
+//server created 
+app.listen(8080, ()=>{
     console.log('server started');
 });
 
--- src/models/Note.js
+++ src/models/Note.js
@@ -4,25 +4,21 @@ const mongoose = require('mongoose');
 
 const noteSchema =    mongoose.Schema(
     {
-
         id:{
             type: String,
             unique: true,
             required: true,
         },
-
         userId:{
             type: String,
             required: true,
 
         },
-
         title:{
             type: String,
             required: true,
 
         },
-
         content:{
             type: String,
         },
@@ -30,7 +26,6 @@ const noteSchema =    mongoose.Schema(
             type:Date,
             default: Date.now,
         }
-
 
     }
 
